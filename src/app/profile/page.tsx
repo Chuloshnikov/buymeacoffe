ef4f78fdde9ba4e3a6160706f98dc55cefae8192
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-05-22 17:28:47 +0300

    done
    server actions added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,29 +1,11 @@
 "use server";
 
-export default async function ProfilePage(args) {
+import ProfileInfoForm from "@/components/ProfileInfoForm";
+
+export default async function ProfilePage() {
     return (
         <div className="max-w-2xl mx-auto px-4">
-            <div className="bg-gray-200 p-4 rounded-lg">
-                <div className="bg-gray-300 size-24 rounded-full p-4">avatar</div>
-                <div>cover image</div>
-            </div>
-            <div>
-                <label className="block mt-4">username</label>
-                <input id="userNameIn" type="text" placeholder="username"/>
-            </div>
-            <div>
-                <label className="block mt-4">display name</label>
-                <input id="displayNameIn" type="text" placeholder="display name"/>
-            </div>
-            <div>
-            <label className="block mt-4">bio</label>
-                <textarea id="bioIn" name="" placeholder="bio" className=""/>
-            </div>
-            <div>
-                <button className="bg-yellow-300 px-4 py-2 rounded-lg mt-4">
-                    Save profile
-                </button>
-            </div>
+            <ProfileInfoForm/>
             <div>
                 donations list...
             </div>
