e6922f908de4b0981647cf3af2e8aa0eaacb69c1
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-06-12 11:45:38 +0300

    done

--- src/actions/donationActions.ts
+++ src/actions/donationActions.ts
@@ -1,24 +1,53 @@
+"use server";
 import { DonationModel } from "@/models/Donation";
+import { ProfileInfoModel } from "@/models/ProfileInfo";
+import axios from "axios";
 import md5 from "md5";
 import mongoose from "mongoose";
 
-export async function createDonation(formData: FormData): Promise<string>{
+export async function createDonation(formData: FormData): Promise<string | false>{
     //save to db
     const {amount, name, message, crypto, email} = Object.fromEntries(formData);
     await mongoose.connect(process.env.MONGODB_URI as string);
     const donationDoc = await DonationModel.create({
         amount, name, message, crypto, email
-    })
-    //create invoice end return the url
+    });
+
+    const profileInfoDoc = await ProfileInfoModel.findOne({email});
+    if (!profileInfoDoc) {
+        return false;
+    }
+    //create a crryptomus invoice end return the url
     const endpoint = 'https://api.cryptomus.com/v1/payment';
     const apiKey = rocess.env.CRIPTOMUS_PAYMENT_API_KEY as string;
     const data = {
-        amount: parseInt(amount as string) * 5,
+        amount: (parseInt(amount as string) * 5).toString() + '.00',
         currency: 'USD',
-        order_id: donationDoc._id,
-        to_currency: crypto,
+        order_id: donationDoc._id.toString(),
+        to_currency: (crypto as string).toUpperCase(),
         url_callback: 'https://mchbuymeacoffee.vercel.app/callback?id='+donationDoc._id,
+        url_return: process.env.NextAuth_URL + '/' + profileInfoDoc.username,
+        url_success: process.env.NEXTAUTH_URL + '/' + profileInfoDoc.username + '?success=1',
     };
-    const sign = md5(btoa(Json.stringify(data)) + apiKey);
-return '';
+
+    const merchant = process.env.CRYPTOMUS_MERCHANT_UUID as string;
+    const sign = md5(btoa(JSON.stringify(data)) + apiKey);
+
+    try {
+        const response = await axios.post(endpoint, data, {
+            headers: {
+                merchant,
+                sign,
+            }
+        });
+
+        return response.data.result.url;
+    } catch (e) {
+    if (axios.isAxiosError(e) && e.response) {
+        console.log(e.response.status);
+        console.log(e.response.data);
+    }
+}
+
+    return false;
 }
---
+++ src/app/callback/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+
+async function handler(req:NextRequest) {
+    console.log({req});
+    return Response.json(true);
+}
+
+export {handler as GET, handler as POST};
